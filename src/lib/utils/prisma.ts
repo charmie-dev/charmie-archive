7ecb455f8df907df394a9a1d7f37b156140dccd2
Dan <130933519+jxstdan@users.noreply.github.com>
2024-08-25 20:35:41 +0200

    fix(cache): Fix guild cache not clearing upon #delete or #deleteMany.

--- src/lib/utils/prisma.ts
+++ src/lib/utils/prisma.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client';
 import { ExtendedPrismaClientType } from '../types';
 
 import GuildCache from '../cache/GuildCache';
+import Logger from './logger';
 
 export const ExtendedPrismaClient = new PrismaClient().$extends({
   query: {
@@ -10,6 +11,16 @@ export const ExtendedPrismaClient = new PrismaClient().$extends({
       async update({ args, query }) {
         const result = await query(args);
         GuildCache.wipeCache(result.id!);
+        return result;
+      },
+      async delete({ args, query }) {
+        const result = await query(args);
+        GuildCache.wipeCache(result.id!);
+        return result;
+      },
+      async deleteMany({ args, query }) {
+        const result = await query(args);
+        GuildCache.wipeAll();
         return result;
       }
     }
