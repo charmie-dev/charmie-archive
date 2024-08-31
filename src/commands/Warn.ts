10f73c62c9f4506c19d85e8ca1a7c759d5f2a3af
dan <130933519+redicides@users.noreply.github.com>
2024-09-01 00:41:31 +0200

    refactor(commands): Revert warning confirmation message to the original format.

--- src/commands/Warn.ts
+++ src/commands/Warn.ts
@@ -81,7 +81,7 @@ export default class Warn extends CharmieCommand {
     return send(message, {
       embeds: [
         {
-          description: `Warning \`#${infraction.id}\` issued for ${target.toString()} (\`${target.id}\`)`,
+          description: `${target.toString()} has been **warned** with ID \`${infraction.id}\``,
           color: INFRACTION_COLORS.Warn
         }
       ]
